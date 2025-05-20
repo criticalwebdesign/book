924ea024bad119821ad297184aa88a5f90fb4ca6
Owen Mundy <omundy@gmail.com>
2025-05-20 09:24:50 -0400

    Adds prompt to 5-2

--- 05-usability/5-2/assets/js/main.js
+++ 05-usability/5-2/assets/js/main.js
@@ -2,15 +2,24 @@ let myForm = document.querySelector("#myForm");
 
 myForm.addEventListener("submit", (event) => {
 
-    // prevent default behavior of form
+	// prevent default behavior of form
 	event.preventDefault();
 
-    // create variables for all items and convert type to numbers
+	// create variables for all items and convert type to numbers
 	let q1 = Number(this.q1.value);
 	let q2 = Number(this.q2.value);
-	
+	// complete these as part of the prompt
+	let q3 = Number(this.q3.value);
+	let q4 = Number(this.q4.value);
+	let q5 = Number(this.q5.value);
+	let q6 = Number(this.q6.value);
+	let q7 = Number(this.q7.value);
+	let q8 = Number(this.q8.value);
+	let q9 = Number(this.q9.value);
+	let q10 = Number(this.q10.value);
+
 	// Add all converted responses
-	let answer = (q1 + q2);
+	let answer = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10);
 
 	// log
 	console.log(answer);
